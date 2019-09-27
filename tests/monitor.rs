238fcdd50429e5579f88a280ce09a540b4859ff5
Andreas Rottmann <a.rottmann@gmx.at>
2019-09-27 16:26:35 +0200

    Add basic test for the mismatch case

--- tests/monitor.rs
+++ tests/monitor.rs
@@ -1,16 +1,29 @@
 use std::time::Duration;
 
 use tdns_update::{
-    record::{RecordSet, RsData},
+    record::RecordSet,
     update::{Settings, Update},
 };
 use tokio::runtime::current_thread::Runtime;
 
 mod mock;
 
-/// This is a basic excercise of the whole happy path.
-#[test]
-fn test_smoke() {
+fn test_settings(expected: &str) -> Settings {
+    Settings {
+        zone: "example.org".parse().unwrap(),
+        entry: "foo.example.org".parse().unwrap(),
+        expected: RecordSet::new(
+            "foo.example.org".parse().unwrap(),
+            expected.parse().unwrap(),
+        ),
+        interval: Duration::from_nanos(100),
+        timeout: Duration::from_millis(10),
+        verbose: true,
+        ..Default::default()
+    }
+}
+
+fn mock_dns(data1: &[(&str, &str, &str)], data2: &[(&str, &str, &str)]) -> mock::Open {
     let rec_data: &[_] = &[
         (
             "example.org",
@@ -28,38 +41,42 @@ fn test_smoke() {
     let master_addr = "192.0.32.162:53".parse().unwrap();
     let auth1_addr = "199.43.135.53:53".parse().unwrap();
     let auth2_addr = "199.43.133.53:53".parse().unwrap();
-    let auth_data = &[("foo.example.org", "A", "192.168.1.1")];
-    let open = mock::Open::new(vec![
+    mock::Open::new(vec![
         (rec_addr, rec_data),
         (master_addr, empty),
-        (auth1_addr, auth_data),
-        (auth2_addr, auth_data),
+        (auth1_addr, data1),
+        (auth2_addr, data2),
     ])
-    .unwrap();
+    .unwrap()
+}
+
+#[test]
+fn test_smoke_match() {
     let mut runtime = Runtime::new().unwrap();
     let update = Update::new(
         runtime.handle(),
-        open,
-        Settings {
-            zone: "example.org".parse().unwrap(),
-            entry: "foo.example.org".parse().unwrap(),
-            expected: RecordSet::new(
-                "foo.example.org".parse().unwrap(),
-                RsData::A(
-                    vec!["192.168.1.1"]
-                        .into_iter()
-                        .map(str::parse)
-                        .collect::<Result<_, _>>()
-                        .unwrap(),
-                ),
-            ),
-            interval: Duration::from_nanos(100),
-            timeout: Duration::from_millis(10),
-            verbose: true,
-            ..Default::default()
-        },
+        mock_dns(
+            &[("foo.example.org", "A", "192.168.1.1")],
+            &[("foo.example.org", "A", "192.168.1.1")],
+        ),
+        test_settings("A:192.168.1.1"),
     )
     .unwrap()
     .run();
     runtime.block_on(update).unwrap();
+}
+
+#[test]
+fn test_smoke_mismatch() {
+    let mut runtime = Runtime::new().unwrap();
+    let update = Update::new(
+        runtime.handle(),
+        mock_dns(&[("foo.example.org", "A", "192.168.1.1")],
+                 &[("foo.example.org", "A", "192.168.1.2")]),
+        test_settings("A:192.168.1.1"),
+    )
+    .unwrap()
+    .run();
+    let result = runtime.block_on(update);
+    assert!(result.is_err()); // TODO: check for timeout error
 }
